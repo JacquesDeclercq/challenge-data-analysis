520ca4f56b26508c2e281f9c09feb73b729ed40b
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-06-29 14:30:29 +0200

    try in jupyter

--- Creating_state_city_columns.py
+++ Creating_state_city_columns.py
@@ -1,14 +1,22 @@
+import pandas as pd
+
+
 def add_cities_provinces_to_dataset(df):
 
-    df.set_index(df["locality"], inplace=True)
-    data_postcodes = "postalcode_city_state.csv"
+    # df.set_index(df["locality"], inplace=True)
+    # df = df.drop(columns='locality')
+
+    data_postcodes = "Dataset\\postalcode_city_state.csv"
     loc_pc = pd.read_csv(data_postcodes, sep=";")
     loc_pc = loc_pc.drop(columns='Unnamed: 3')
     loc_pc = loc_pc.drop(columns='Unnamed: 4')
+    loc_pc = loc_pc.drop_duplicates(subset=['Postcode'])
+    print(loc_pc.Postcode.value_counts())
 
-    merged_df = df.merge(loc_pc, left_index=True, right_on="Postcode")
-    merged_df = merged_df.reset_index()
-    merged_df = merged_df.drop(columns='locality')
-    merged_df = merged_df.rename(columns={'Postcode':'locality'})
+    df = df.merge(loc_pc, how='inner', left_on='locality', right_on="Postcode")
+    df = df.rename(columns={'City': 'city',
+                            'State': 'province'})
+    df = df.drop(columns='Postcode')
+    print(df.shape)
 
-    return merged_df
+    return df
