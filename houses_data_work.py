f62d217ed3217e45abab414047135fd7a7c36c4a
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-06-29 12:09:57 +0200

    before plotting

--- houses_data_work.py
+++ houses_data_work.py
@@ -3,32 +3,81 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from Creating_state_city_columns import add_cities_provinces_to_dataset
+
 path = 'Dataset\\final_list_houses_dataset.csv'
 houses_data = pd.read_csv(path, sep=',')
 # copy dataframe
 houses_copy = houses_data.copy()
+print(houses_copy.columns)
+
 houses_copy = houses_copy.drop(columns='Unnamed: 0')
+# del houses_copy[houses_copy['type of property']]
+# houses_copy = houses_copy.drop(columns='open fire')
+
+def make_postal_code_second_attempt(df):
+    # Create column "province"
+    df["province"] = ''
+    for index, row in df.iterrows():
+        if (row["locality"] >= 1000 & row["locality"] <= 1299):
+            row["province"] = "Brussels-Capital"
+            continue
+        elif (row["locality"] >= 1300 & row["locality"] <= 1499):
+            row["province"] = "Walloon Brabant"
+            continue
+        elif (row["locality"] >= 1500 & row["locality"] <= 1599):
+            row["province"] = "Flemish Brabant"
+            continue
+        elif (row["locality"] >= 2000 & row["locality"] <= 2999):
+            row["province"] = "Antwerp"
+            continue
+        elif (row["locality"] >= 3000 & row["locality"] <= 3399):
+            row["province"] = "Flemish Brabant"
+            continue
+        elif (row["locality"] >= 3500 & row["locality"] <= 3599):
+            row["province"] = "Limburg"
+            continue
+        elif (row["locality"] >= 4000 & row["locality"] <= 4999):
+            row["province"] = "Liège"
+            continue
+        elif (row["locality"] >= 5000 & row["locality"] <= 5999):
+            row["province"] = "Namur"
+            continue
+        elif (row["locality"] >= 6000 & row["locality"] <= 6599):
+            row["province"] = "Hainaut"
+            continue
+        elif (row["locality"] >= 6600 & row["locality"] <= 6999):
+            row["province"] = "Luxembourg"
+            continue
+        elif (row["locality"] >= 7000 & row["locality"] <= 7799):
+            row["province"] = "Hainaut"
+            continue
+        elif (row["locality"] >= 8000 & row["locality"] <= 8899):
+            row["province"] = "West Flanders"
+            continue
+
+    return df
 
 
 def make_postal_code_qualitative_value(dataframe):
     # Create column "province"
-    dataframe["province"] = np.nan
-
+    dataframe["province"] = ' '
     df = dataframe.sort_values('locality')
 
-    for row in df['locality']:
+    # for row in df['locality']:
+    for index, row in df.iterrows():
+        # if df.loc[row, 'locality'] >= 1000 & df.loc[row, 'locality'] <= 1299:
+        #     df.loc[row, "province"] = "Brussels-Capital"
 
-    # for index, row in df.iterrows():
-        if df.loc[row, 'locality'] >= 1000 & df.loc[row, 'locality'] <= 1299:
-            df.loc[row, "province"] = "Brussels-Capital"
-
-        # if df[row['locality']] >= 1000 & df[row['locality']] <= 1299:
-        #     df[row['province']] = "Brussels-Capital"
-
-        elif df.loc[row, 'locality']>= 1300 & df.loc[row, "locality"] <= 1499:
-            df.loc[row, "province"] = "Walloon Brabant"
-        elif df.loc[row, 'locality']>= 1500 & df.loc[row, "locality"] <= 1999:
-            df.loc[row, "province"] = "Flemish Brabant"
+        if row['locality']>= 1000 and row['locality']<= 1299:
+            # row['province'] = "Brussels-Capital"
+            # df.loc[index, "province"] = "Brussels-Capital"
+            df.at[index, 'province'] = "Brussels-Capital"
+            continue
+        # elif df.loc[row, 'locality']>= 1300 & df.loc[row, "locality"] <= 1499:
+        #     df.loc[row, "province"] = "Walloon Brabant"
+        # elif df.loc[row, 'locality']>= 1500 & df.loc[row, "locality"] <= 1999:
+        #     df.loc[row, "province"] = "Flemish Brabant"
         # elif (row["locality"] >= 2000 & row["locality"] <= 2999):
         #     row["province"] = "Antwerp"
         # elif (row["locality"] >= 3000 & row["locality"] <= 3499):
@@ -51,6 +100,7 @@ def make_postal_code_qualitative_value(dataframe):
         #     row["province"] = "East Flanders"
 
         return df
+
 
 def rename_column_titles(df):
     df = df.rename(columns={'Price [€]': 'price',
@@ -98,6 +148,7 @@ def convert_price_from_string_to_int(df):
 
 def print_column_values(df, column):
     print("possible values in ", column, df[column].unique())
+    print("possible values in ", column, df[column].value_counts('Brussels-Capital'))
 
 
 def print_column_na_count(df, column):
@@ -116,11 +167,19 @@ def show_lmplot(x_value, y_value, data_value, hue_value):
 
 
 houses_copy = rename_column_titles(houses_copy)
-houses_copy = make_postal_code_qualitative_value(houses_copy)
+# houses_copy = make_postal_code_qualitative_value(houses_copy)
+houses_copy = add_cities_provinces_to_dataset(houses_copy)
+
+
+# houses_copy = make_postal_code_second_attempt(houses_copy)
+# print(houses_copy.head())
+
 # print(houses_copy.columns)
-print(houses_copy['province'].head())
-print(print_column_values(houses_copy, 'province'))
-print(houses_copy.groupby('province').count())
+# print(houses_copy['province'].head())
+# print(print_column_values(houses_copy, 'province'))
+# print(houses_copy.tail())
+# print(houses_copy.columns)
+# print(houses_copy.groupby('province').count())
 # print_duplicate_houses(houses_copy)
 houses_copy = drop_duplicate_columns(houses_copy)
 strip_strings(houses_copy)
@@ -179,3 +238,5 @@ houses_copy = houses_copy[houses_copy['subtype of property'] == 'house']
 subtype_house_copy = houses_copy[houses_copy['subtype of property'] == 'house']
 # print(subtype_house_copy.head())
 # print(subtype_house_copy.shape)
+
+subtype_house_copy.info()
